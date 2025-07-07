37eb313213bf92388b1dcf761e9fefcd4a764b23
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-07-07 19:48:05 -0300

    start adding java service

---
+++ apps/books-service/src/main/java/com/multilanguage_microservices_study/books_service/BooksServiceApplication.java
@@ -0,0 +1,14 @@
+package com.multilanguage_microservices_study.books_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksServiceApplication.class, args);
+	}
+
+
+}
---
+++ apps/books-service/src/test/java/com/multilanguage_microservices_study/books_service/BooksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multilanguage_microservices_study.books_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
